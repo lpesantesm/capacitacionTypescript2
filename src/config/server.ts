a80889ab47f6954146775775cb23485dfa6d2846
Luis Pesantes <lpesantesm@gmail.com>
2024-09-20 23:09:43 -0500

    Mejoras del validador

--- src/config/server.ts
+++ src/config/server.ts
@@ -1,12 +1,19 @@
-import  { cleanEnv, host, port, str }  from "envalid"
+import  { cleanEnv, host, makeValidator, port }  from "envalid"
 import dotenv from "dotenv"
 
 dotenv.config()
+
+const minLength = makeValidator((value: string) =>{
+    if(value.length <=15){
+        throw new Error ('La variable debe tener minimo 15 caracteres')
+    }
+    return value
+})
 
 export const config = {
     server: cleanEnv(process.env ,{
         HOST: host(),
         PORT: port(),
-        JWT_SECRET: str()
+        JWT_SECRET: minLength()
     })
 }
